a74d3b754182611ef2460e693560f1e6fe18d3b3
Anna Koberidze <AnnaKoberidze1@gmail.com>
2023-01-13 21:27:25 +0400

    first

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React from "react";
-import Carrousel, { CarrouselItem } from "./components/carrousel/carrousel";
+import Carrousel from "./components/carrousel/carrousel";
 import Header from "./components/header";
-import Slider from "./components/slider/Slider";
 
 function App() {
   return (
     <div className="App">
-      <Slider />
-      <Carrousel>
-        <CarrouselItem>item 1</CarrouselItem>
-        <CarrouselItem>item 2</CarrouselItem>
-        <CarrouselItem>item 3</CarrouselItem>
-      </Carrousel>
+      <Carrousel />
       <Header />
     </div>
   );
--- src/components/carrousel/Data.js
+++ src/components/carrousel/Data.js
@@ -2,9 +2,10 @@ export const sliderData = [
   {
     image: "/images/image-1.jpg",
     heading: "Welcome",
-    desc: "lorem ipsum dolor",
+    desc: "Welcome to my portfolio page",
     button: "Get started",
     btnClassName: "-start",
+    cntClassName: "-welcome",
   },
   {
     image: "/images/image-2.jpg",
@@ -12,6 +13,7 @@ export const sliderData = [
     desc: "lorem ipsum dolor",
     button: "resume",
     btnClassName: "-resume",
+    cntClassName: "-my-resume",
   },
   {
     image: "/images/image-3.jpg",
@@ -19,6 +21,7 @@ export const sliderData = [
     desc: "lorem ipsum dolor",
     button: "projects",
     btnClassName: "-projects",
+    cntClassName: "-my-projects",
   },
   {
     image: "/images/image-4.jpg",
@@ -26,5 +29,6 @@ export const sliderData = [
     desc: "lorem ipsum dolor",
     button: "contact",
     btnClassName: "-contact",
+    cntClassName: "-my-contacts",
   },
 ];
