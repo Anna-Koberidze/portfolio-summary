a3545ad0b943503763adac3bbc2cfef446ae8e2b
Anna Koberidze <AnnaKoberidze1@gmail.com>
2023-02-06 22:32:33 +0400

    home

---
+++ src/pages/Home/header/header.jsx
@@ -0,0 +1,16 @@
+function Header() {
+  return (
+    <header>
+      <h1>Hello, I'm Anna. Nice to meet you.</h1>
+      <div>
+        Since the beginning of my journey as front-end developer I endeavor to
+        learn everything I can get my hands on and become proficient in the
+        field of web developement. I am gaining confidence in my abilities and
+        perpetually working on improving my skills solving problem at a time. I
+        am ready to take part in creating customer oriented new solutions and
+        innovative products.
+      </div>
+    </header>
+  );
+}
+export default Header;
--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,17 +1,15 @@
 import Carrousel from "../../components/Nav/Nav-components/carrousel/carrousel";
-import Header from "../../components/header";
+import Header from "./header/header";
+import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <div>
       <Carrousel />
-
-      {/* <Header />
       <Header />
-      <Header />
-      <Header />
-      <Header />
-      <Header />
-      <section id="--section1"></section> */}
+      <section id="--section1"></section>
+      <button>
+        <Link to="/Resume">next→</Link>
+      </button>
     </div>
   );
 };
