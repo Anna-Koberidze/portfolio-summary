34a8c1167d18a923c1d11ef51e8a97d1a35f6e9a
Anna Koberidze <AnnaKoberidze1@gmail.com>
2023-02-07 18:22:16 +0400

    animations added to text

--- src/pages/Home/header/header.jsx
+++ src/pages/Home/header/header.jsx
@@ -1,17 +1,31 @@
 import { Link } from "react-router-dom";
+import { useEffect } from "react";
+import Aos from "aos";
+import "aos/dist/aos.css";
 function Header() {
+  useEffect(() => {
+    Aos.init();
+  }, []);
   return (
     <header className="header-1">
-      <h1 className="greeting">
+      <h1
+        data-aos="fade-right"
+        data-aos-easing="ease-in-sine"
+        data-aos-duration="2000"
+        className="greeting"
+      >
         Hello, I'm Anna,<br></br> I'm a Junior front-end developer. <br></br>
         Nice to meet you.
       </h1>
       <img
+        data-aos="fade-left"
+        data-aos-duration="3000"
+        data-aos-easing="ease-in-sine"
         className="header-photo"
         alt="photo illustration"
         src="/images/undraw_Blooming_re_2kc4.png"
       />
-      <div className="introduction">
+      <div data-aos="zoom-in" className="introduction">
         Since the beginning of my journey as front-end developer I endeavor to
         learn everything I can get my hands on and become proficient in the
         field of web developement. <br></br>
