2a7fbec574f5dd6736eb7be16089d3baf5ca66d3
Anna Koberidze <AnnaKoberidze1@gmail.com>
2023-02-08 20:57:53 +0400

    new

--- src/components/Nav/Nav-components/carrousel/carousel-component.js
+++ src/components/Nav/Nav-components/carrousel/carousel-component.js
@@ -10,8 +10,8 @@ import "./Carousel.scss";
 import { Link } from "react-router-dom";
 const Carousel = () => {
   return (
-    <CCarousel controls indicators interval pause="hover" touch="true">
-      <CCarouselItem interval="true" className="--carousel-item">
+    <CCarousel controls indicators interval="true" pause="true">
+      <CCarouselItem interval="3000" className="--carousel-item">
         <CImage
           className="d-block w-100"
           src={sliderData[0].image}
@@ -27,7 +27,7 @@ const Carousel = () => {
           </button>
         </CCarouselCaption>
       </CCarouselItem>
-      <CCarouselItem interval="true" className="--carousel-item">
+      <CCarouselItem interval="3000" className="--carousel-item">
         <CImage
           className="d-block w-100"
           src={sliderData[1].image}
@@ -43,7 +43,7 @@ const Carousel = () => {
           </button>
         </CCarouselCaption>
       </CCarouselItem>
-      <CCarouselItem interval="true" className="--carousel-item">
+      <CCarouselItem interval="3000" className="--carousel-item">
         <CImage
           className="d-block w-100"
           src={sliderData[2].image}
@@ -59,7 +59,7 @@ const Carousel = () => {
           </button>
         </CCarouselCaption>
       </CCarouselItem>
-      <CCarouselItem interval="true" className="--carousel-item">
+      <CCarouselItem interval="3000" className="--carousel-item">
         <CImage
           className="d-block w-100"
           src={sliderData[3].image}
