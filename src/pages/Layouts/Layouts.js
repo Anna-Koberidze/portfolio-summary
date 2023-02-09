59fd536d17c5b1ce50f3072974ed5ab2f8f4b0df
Anna Koberidze <AnnaKoberidze1@gmail.com>
2023-02-09 17:48:58 +0400

    some picture changes

--- src/pages/Layouts/Layouts.js
+++ src/pages/Layouts/Layouts.js
@@ -4,30 +4,32 @@ import "../Contacts/Contacts.scss";
 const Layout = () => {
   return (
     <>
-      <nav>
-        <div className="first-layout-div">
-          <Link to="/" className="layout-link">
-            Home
-          </Link>
-        </div>
+      <div className="navigation-bar-container">
+        <nav>
+          <div className="first-layout-div">
+            <Link to="/" className="layout-link">
+              Home
+            </Link>
+          </div>
 
-        <div>
-          <Link to="/Resume" className="layout-link">
-            Resume
-          </Link>
-        </div>
-        <div>
-          <Link to="/Projects" className="layout-link">
-            Projects
-          </Link>
-        </div>
+          <div>
+            <Link to="/Resume" className="layout-link">
+              Resume
+            </Link>
+          </div>
+          <div>
+            <Link to="/Projects" className="layout-link">
+              Projects
+            </Link>
+          </div>
 
-        <div>
-          <Link to="/Contact" className="layout-link">
-            Contact
-          </Link>
-        </div>
-      </nav>
+          <div>
+            <Link to="/Contact" className="layout-link">
+              Contact
+            </Link>
+          </div>
+        </nav>
+      </div>
 
       <Outlet />
     </>
