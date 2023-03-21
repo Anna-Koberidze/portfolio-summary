6bb6227ca565c8acfd2564ca7637992bac4f22df
Anna Koberidze <AnnaKoberidze1@gmail.com>
2023-03-21 19:07:30 +0400

    changed images into links and changed team chooser

--- src/pages/Projects/ind-projects.js
+++ src/pages/Projects/ind-projects.js
@@ -1,7 +1,9 @@
 function ProjectCard(props) {
   return (
     <div key={props.index} className={`p-cards ${props.className}`}>
-      <img alt="screenshot of website" src={props.img} />
+      <a target="_blank" rel="noreferrer" href={props.link}>
+        <img alt="screenshot of website" src={props.img} />
+      </a>
       <h2>{props.name}</h2>
       <p>{props.desc}</p>
       <button className="project-button">
